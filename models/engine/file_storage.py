5c1987a0bc69a50a667ce2bfe517a2da5ca6fcb8
timishot <timidotcom58@gmail.com>
2023-11-24 23:31:49 +0100

    update the all instance inr the file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
+
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dict = {}
+        if cls:
+            dictionary = FileStorage.__objects
+            for key in dictionary:
+                partition = key.replace(".", " ").split()
+                if (partition[0] == cls.__name__):
+                    dict[key] = FileStorage.__objects[key]
+            return (dict)
+        else:
+            return FileStorage.__objects; 
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +58,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete an exisiting elemen"""
+        if obj:
+            key="{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
