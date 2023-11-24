f42790ebbd441ce20c360785f437b223ec059b3c
timishot <timidotcom58@gmail.com>
2023-11-24 22:14:24 +0100

    git push

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,5 @@
--- Create or use the database hbnb_test_db
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create or use the user hbnb_test, set the password to hbnb_test_pwd
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges on hbnb_test_db to hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grant SELECT privilege on performance_schema to hbnb_test
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Prepares a MySQL server for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
