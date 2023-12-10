d9b13c06a7d3665e0c837dbd2c3355c622f6e323
timishot <timidotcom58@gmail.com>
2023-12-10 23:09:25 +0100

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" using fabric 101
-"""
+"""Fabric script that generates a .tgz archive"""
+from fabric.api import local
 from datetime import datetime
-from fabric import task
-from invoke import run
-from os.path import isdir
 
 
-@task
-def do_pack(ctx):
-    """generates a tgz achive"""
+def do_pack():
+    """Generates a .tgz archive from web_static"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("version") is False:
-            run("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        run("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception as e:
+        current_time = datetime.utcnow()
+        formatted_time = current_time.strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(formatted_time)
+
+        local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_path))
+
+        print("web_static packed: {} -> {}Bytes".format(archive_path, local(
+              "du -b {}".format(archive_path), capture=True)))
+        return archive_path
+    except Exception:
         return None
