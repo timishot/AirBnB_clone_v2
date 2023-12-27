92d9176106030d0fdd50c52fd9c6c3d6a176c703
Timishot <timidotcom58@gmail.com>
2023-12-27 21:02:08 +0100

    fix two

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,11 +10,14 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
