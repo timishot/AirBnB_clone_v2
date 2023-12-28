64564448675c7ff1a3625e6cd0dd3bee08edee6c
Timishot <timidotcom58@gmail.com>
2023-12-28 21:48:16 +0100

    Fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,16 @@
--- Prepares a MySQL server for the project.
+-- Create or use the existing database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Tomihin@1';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+
+-- Create or use the existing user hbnb_dev for localhost
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+
+
+-- Display the current grants for hbnb_dev
+SHOW GRANTS FOR 'hbnb_dev'@'localhost';
