6ec3045117b98c6e4fed994c5303910e87ba2f8f
Timishot <timidotcom58@gmail.com>
2023-12-28 15:05:08 +0100

    Push

--- main.py
+++ main.py
@@ -34,4 +34,5 @@ print("")
 all_states = storage.all(State)
 for state_id, state in all_states.items():
     for city in state.cities:
+        print('hi')
         print("Find the city {} in the state {}".format(city, state))
