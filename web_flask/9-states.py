72abcc2903d7cdcdd6bd04941a3a9107abe8f2f4
Timishot <timidotcom58@gmail.com>
2023-12-30 13:52:34 +0100

    Implemented pystylecode

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,6 +10,7 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/states", strict_slashes=False)
 def states():
     """Displays an HTML page with a list of all States.
@@ -18,21 +19,21 @@ def states():
     states = storage.all(State)
     return render_template("9-states.html", state=states)
 
+
 @app.route("/states/<id>", strict_slashes=False)
-def cities(id=None):
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    for state in storage.all(State).values():
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
         if state.id == id:
-            return render_template("9-states.html", state= state)
-        return render_template("9-states.html")
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
